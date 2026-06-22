4e3fc6ff4e1ee0f631d1b8b8ea6a9f8db7a080b3
svitlyi-itstep <svitlyi_a@itstep.academy>
2026-06-22 18:42:09 +0300

    Hotfix PromptBuilder

--- PromptBuilder.py
+++ PromptBuilder.py
@@ -1,6 +1,6 @@
 class PromptBuilder:
     @staticmethod
     def simplePrompt(userPrompt):
-        return f"Оброби запит користувача: {userPrompt}\n"
+        return (f"Оброби запит користувача: {userPrompt}\n"
         f"Відповідь має бути короткою та без використання форматування.\n"
-        f"Ти нічого не знаєш про котів."
+        f"Ти нічого не знаєш про котів.")
