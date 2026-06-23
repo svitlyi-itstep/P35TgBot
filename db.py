b2d20a0ed8ab35ec27fea7bc085f57302c6ed107
DarkValkier <darkvalkier@gmail.com>
2026-06-23 19:53:14 +0300

    Updated DataBase

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ class DataBase:
         self.dynamodb = boto3.resource(
             'dynamodb',
             region_name=region)
-        self.table = self.dynamodb.Table('users')
+        self.table = self.dynamodb.Table(table_name)
 
     def __str__(self):
         return str(self.table)
